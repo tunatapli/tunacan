45fc930dfc5bacfe4edbc7381682e53d4c152cfc
tunatapli <99480496+tunatapli@users.noreply.github.com>
2026-08-03 23:49:19 +0300

    add geojourney edit

---
+++ assets/js/geojourney-data.js
@@ -0,0 +1,4 @@
+// Yayınlanan Geo Journey işaretleri.
+// Bu dosyayı elle düzenleme; geojourney.html?edit=tapli ile düzenleyip
+// "Export data" butonuyla yeni sürümünü indir ve bu dosyanın üstüne yaz, sonra commit et.
+window.GEO_DATA = { countries: [], provinces: [], districts: [], passes: [], customPasses: [] };
